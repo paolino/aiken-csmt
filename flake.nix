a3f6a0c0ce8beb371d41cf7ea56b45d0c063dc4a
Paolo Veronelli <paolo.veronelli@gmail.com>
2026-03-05 10:20:47 +0000

    fix: pin haskell-mts flake input to specific commit

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    haskell-mts.url = "github:paolino/haskell-mts";
+    haskell-mts.url = "github:paolino/haskell-mts/4cd37b80248b218e8f4c2fda8fc6a817e396a407";
   };
 
   outputs =
